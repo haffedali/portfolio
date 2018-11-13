fb3369089d45c95c331170a1e649ab0d48e505be
Hoff <haffedmcnair@gmail.com>
2018-11-12 16:10:40 -0800

    added yarn router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects/Projects.js";
 import Contact from "./components/Contact";
