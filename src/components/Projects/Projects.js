a305b7a3b71b2e48102db68a2344403326a7447f
Hoff <haffedmcnair@gmail.com>
2018-12-04 15:43:27 -0800

    added bamazon project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,6 +19,12 @@ const Projects = () => {
             url = "https://stormy-brushlands-10294.herokuapp.com/?fbclid=IwAR3axaknsToSN29k2CIIjPIS0zaARDyoeZ5TOfp9EkWJw7po-zGE5Nhvth8"
             blurb = "A social networking site where artists find, inspire, and collaborate! This is a project I truly care about and use myself!"
         />
+        <Slider 
+            title = "Bamazon"
+            thumbnail = "https://cdn-images-1.medium.com/max/1200/1*BK36TSiJLI48i2ZM6ozFdg.jpeg"
+            codeUrl = "https://github.com/haffedali/bamazon"
+            blurb = "This is a command line interface Node project. Manage your inventory in the manager view, and make your purchases in the customer view!"
+        />
         <Slider
             title = "And more to come!"
             thumbnail = "https://c7.uihere.com/files/296/575/586/plus-and-minus-signs-computer-icons-clip-art-plus-sign.jpg"
