e764e105d1b060490c2e60b1678df365ed7efea2
Hoff <haffedmcnair@gmail.com>
2019-06-06 09:53:20 -0700

    fix yo image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,19 +5,21 @@ const About = () => (
     <div className="container container-about">
         <div className="about">
             <div className="portrait">
-                <img className="selfie"src="https://media.licdn.com/dms/image/C5603AQFVGZIChN2Zwg/profile-displayphoto-shrink_200_200/0?e=1548288000&v=beta&t=XLwKfbau2_IfPcOheJ3e5ykgMsqdamJW6sbfeHErfRA"/>
+                <img className="selfie"src="https://media.licdn.com/dms/image/C5603AQFVGZIChN2Zwg/profile-displayphoto-shrink_200_200/0?e=1548288000&v=beta&t=XLwKfbau2_IfPcOheJ3e5ykgMsqdamJW6sbfeHErfRA" alt="Supposed to be my handsome face here... woops"/>
             </div>
             <div className="testomonial">
-                <p className="aboutText">My name is Haffed Ali and I have a passion for development and learning. I have finished Berkeley's Web Dev bootcamp in Fall 2018. During the three month course, I learned and practiced the MERN stack, and was able to meet and work with a lot of like minded people! I value teamwork, and am a firm believer of the adage "two heads are better than one".<br /><br />
+                
+                    <p className="aboutText">My name is Haffed Ali and I have a passion for development and learning. I have finished Berkeley's Web Dev bootcamp in Fall 2018. During the three month course, I learned and practiced the MERN stack, and was able to meet and work with a lot of like minded people! I value teamwork, and am a firm believer of the adage "two heads are better than one".<br /><br />
 
 
-During the bootcamp, it allowed me to explore a few projects that required working with several different team members (located in the 'Projects' tab). Those opportunities allowed me to understand my interpersonal skills such as communication, time management, and pair programming. Of course outside of soft skills, I learned a lot about relevant tech and frameworks and am learning more every day.<br /><br />
+                    During the bootcamp, it allowed me to explore a few projects that required working with several different team members (located in the 'Projects' tab). Those opportunities allowed me to understand my interpersonal skills such as communication, time management, and pair programming. Of course outside of soft skills, I learned a lot about relevant tech and frameworks and am learning more every day.<br /><br />
 
 
-I am striving to work in the back end side of development, as I find it my strongest suit and I love making information, data, or products available to a wide range of people . I pride myself in my ability to study effectively, and the energy and passion I can bring to projects I believe in. I am looking for a place where I can grow professionally and continue my growth as a developer.
+                    I am striving to work in the back end side of development, as I find it my strongest suit and I love making information, data, or products available to a wide range of people . I pride myself in my ability to study effectively, and the energy and passion I can bring to projects I believe in. I am looking for a place where I can grow professionally and continue my growth as a developer.
 
 
-                </p>
+                    </p>
+                
             </div>
         </div>
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => {
     return(
-        <div id="container">
+        <div id="footer-container">
             <div className="linkContainer">
                 <div class="link"><a href="https://www.linkedin.com/in/haffed-ali-991b1916b/" target="_blank"><i class="fab fa-linkedin"></i></a></div>
                 <div class="link"><a href="https://github.com/haffedali" target="_blank"><i class="fab fa-github-square"></i></a></div>
